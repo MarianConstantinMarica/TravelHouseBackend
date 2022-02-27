0624752067bae946d9ae6988bed5ee0aa27b0feb
Marian-Constantin Marica <marian.constantin.marica@gmail.com>
2022-02-27 14:16:25 +0200

    Autowire UserService and PasswordEncoder to SecurityConfig

--- src/main/java/marian/constantin/marica/travelhousebackend/security/SecurityConfig.java
+++ src/main/java/marian/constantin/marica/travelhousebackend/security/SecurityConfig.java
@@ -1,8 +1,19 @@
 package marian.constantin.marica.travelhousebackend.security;
 
+import marian.constantin.marica.travelhousebackend.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(UserService userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
 }
