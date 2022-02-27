980dc931424319af3dfc2e16a9b2690d3af15919
Marian-Constantin Marica <marian.constantin.marica@gmail.com>
2022-02-27 13:28:22 +0200

    Create Spring Boot Project

---
+++ src/main/java/marian/constantin/marica/travelhousebackend/TravelHouseBackendApplication.java
@@ -0,0 +1,13 @@
+package marian.constantin.marica.travelhousebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelHouseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelHouseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/marian/constantin/marica/travelhousebackend/TravelHouseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package marian.constantin.marica.travelhousebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelHouseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
