ef6c841cd88bf34b6bd82804314ce20362cb55e2
Marian-Constantin Marica <marian.constantin.marica@gmail.com>
2022-03-16 14:22:18 +0200

    Create User controller

---
+++ src/main/java/marian/constantin/marica/travelhousebackend/controller/UserController.java
@@ -0,0 +1,9 @@
+package marian.constantin.marica.travelhousebackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/user")
+public class UserController {
+}
