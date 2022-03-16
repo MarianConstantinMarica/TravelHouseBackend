0dadad13bdecfdf92e8be13c5cd3a14fbd02d264
Marian-Constantin Marica <marian.constantin.marica@gmail.com>
2022-03-16 14:26:52 +0200

    Autowire User controller, service and repository

--- src/main/java/marian/constantin/marica/travelhousebackend/controller/UserController.java
+++ src/main/java/marian/constantin/marica/travelhousebackend/controller/UserController.java
@@ -1,9 +1,18 @@
 package marian.constantin.marica.travelhousebackend.controller;
 
+import marian.constantin.marica.travelhousebackend.service.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/v1/user")
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
--- src/main/java/marian/constantin/marica/travelhousebackend/service/user/UserServiceImpl.java
+++ src/main/java/marian/constantin/marica/travelhousebackend/service/user/UserServiceImpl.java
@@ -1,7 +1,16 @@
 package marian.constantin.marica.travelhousebackend.service.user;
 
+import marian.constantin.marica.travelhousebackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
